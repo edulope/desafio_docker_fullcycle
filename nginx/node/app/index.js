bbec1abe85c56bdb6dcd526252dea979715c8d04
Joao <jp-dealmeida@hotmail.com>
2025-10-01 11:28:58 -0300

    commit inicial

---
+++ nginx/node/app/index.js
@@ -0,0 +1,30 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+const config = {
+    host: 'db',
+    user: 'root',
+    password: 'root',
+    database:'nodedb'
+};
+const mysql = require('mysql')
+const connection = mysql.createConnection(config)
+
+const sql = `INSERT INTO people(name) values('Wesley')`
+connection.query(sql)
+connection.end()
+
+
+app.get('/', (req,res) => {
+    const connection = mysql.createConnection(config);
+    connection.query("SELECT name FROM people", function (err, result, fields) {
+        if (err) throw err;
+        res.send('<h1>Full Cycle</h1>' + '\n' + JSON.stringify(result))
+    });
+    connection.end();
+})
+
+app.listen(port, ()=> {
+    console.log('Rodando na porta ' + port)
+})
